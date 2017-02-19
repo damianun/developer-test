7975121ea296f7a6a467e31110b7bd45299170a1
damian <damianun@gmail.com>
2017-02-19 19:28:52 +0000

    Adding test for checking buyer id when making an offer. Test fails

--- OrangeBricks.Web.Tests/Controllers/Property/Commands/MakeOfferCommandHandlerTest.cs
+++ OrangeBricks.Web.Tests/Controllers/Property/Commands/MakeOfferCommandHandlerTest.cs
@@ -81,5 +81,19 @@ namespace OrangeBricks.Web.Tests.Controllers.Property.Commands
             Assert.IsTrue(property.Offers.FirstOrDefault(x => x.UpdatedAt != default(DateTime)) != null, "An offer with UpdateAt date is missing");
             Assert.IsTrue(property.Offers.FirstOrDefault(x => x.CreatedAt != default(DateTime)) != null, "An offer with CreateAt date is missing");
         }
+
+        [Test]
+        public void HandlerShouldCreateAnOfferWithBuyerReferenceSet()
+        {
+            var command = new MakeOfferCommand { Offer = 9000, PropertyId = 1, BuyerUserId = "ID"};
+
+            var property = new Models.Property();
+
+            _properties.Find(1).Returns(property);
+
+            _handler.Handle(command);
+
+            Assert.IsTrue(property.Offers.FirstOrDefault(x => !string.IsNullOrEmpty(x.BuyerUserId)) != null, "Buyer reference is missing on offer");
+        }
     }
 }
